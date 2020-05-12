a829088c64e6001fa6f5914315fbe44c403e69ca
ss-xable <61976458+ss-xable@users.noreply.github.com>
2020-05-12 21:12:01 +0200

    Update Task 4.py

--- Task 4.py
+++ Task 4.py
@@ -1,6 +1,7 @@
 import random
 import sys
 
+# Writing staff details. 
 staff_details = open('staff.txt', 'w')
 staff_details.write("Username1: jsmith\n")
 staff_details.write("Password1: Qwerty12\n")
@@ -13,6 +14,7 @@ staff_details.write("Email2: dorcasstinson@yahoo.com\n")
 staff_details.write("Fullname2: Dorcas Stinson\n")
 staff_details.close()
 
+#The staff login page of the program. 
 yes = True
 while yes:
     print("=====================================")
@@ -22,7 +24,8 @@ while yes:
     start = True
     while start:
         if choice == 'YES':
-
+            
+#Reading the staff.txt file. 
             staff_details = open('staff.txt', 'r')
             staff_username = str(input('Enter username: '))
             staff_password = str(input('Enter password: '))
@@ -52,6 +55,7 @@ while yes:
                 staff_details.close()
         else:
             sys.exit()
+#Creatng Session file.
 
     choice = True
     while choice:
@@ -60,14 +64,18 @@ while yes:
         login_successfully = int(input(
             "Please select your preferred option:" "\n1. To Create New Bank Account" "\n2. To Check Account Details" "\n3. To Logout \n=> "))
 
+#Creating new bank account
         if login_successfully == 1:
             account_name = str(input('Enter Account Name: ')).lower()
             account_balance = str(input('Enter Opening Balance: '))
             account_type = str(input('Account Type: '))
             account_email = str(input('Account Email: '))
 
+#Creating account number
             account_number = ''.join((map(str, [random.randint(1, 9) for i in range(0, 10)])))
             print()
+        
+#Writing customer details.
             customer_details = open('customer.txt', 'w')
             customer_details.write("Account Name: %s \n" % account_name)
             customer_details.write("Opening Balance: %s \n" % account_balance)
@@ -81,6 +89,7 @@ while yes:
             customer_details.close()
             choice = True
 
+#To check account number            
         if login_successfully == 2:
             check = True
             while check:
@@ -98,6 +107,7 @@ while yes:
                     print()
                     check = True
 
+#To log out.                     
         if login_successfully == 3:
             print()
             choice = False
